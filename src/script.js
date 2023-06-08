2c512f1cb0ae3fe6668f763075bf1d0e87c332df
Hamza Gabajiwala <hamzajg16@gmail.com>
2023-06-08 21:51:55 +0100

    Updated html

---
+++ src/script.js
@@ -0,0 +1,21 @@
+const message = 'Hello world';
+
+var myHeader = document.querySelector('#header')
+
+function genColor(){
+  return Math.floor(Math.random() * 255);
+}
+
+function getRGB(){
+  r = genColor();
+  g = genColor();
+  b = genColor();
+  return "rgb("+r+","+g+","+b+")";
+}
+
+function changeHeaderColor(){
+  colorInput = getRGB();
+  myHeader.style.color = colorInput;
+}
+
+setInterval("changeHeaderColor()", 250);
